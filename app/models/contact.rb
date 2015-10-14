29bb3d7ded31cf34842c9065ef274e8cc317c54c
Bill Ma <mqyznb@gmail.com>
2015-10-14 15:45:53 -0400

    sd

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "My Contact Form",
-      :to => "bill.m@zbestpayment.com",
+      :to => "mqyznb@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end
